41bb8ed203e5c22dcf219b9e439c26745105048a
Prakhar <prakhar@Prakhar-MacMini.local>
2020-09-20 15:32:25 +0530

    Updated styles.

--- Components/src/Styles.js
+++ Components/src/Styles.js
@@ -1,18 +1,6 @@
 export const COLOURS = {
   primary: '#dadada',
-  darkGray: '#383838',
-  lightGray: '#808080',
-  lightestGray: '#cdcdcd',
-  grayText: '#575757',
-  blueTextColor: '#005EE4',
   fontColorWhite: 'white',
-  fontColorBlack: 'black',
-  fontColorOrangeDark: '#E85E11',
-  fontColorOrange: '#FE752F',
-  fontColorViolet: '#B6BAC5', //light
-  fontColorBlue: '#00AAFF',
-  fontColorMaroon: '#521301',
-  fontColorBrown: '#210801',
   colorBackgroundBlack: '#0f0f0f',
   colorTicketGray: '#404040'
 };
